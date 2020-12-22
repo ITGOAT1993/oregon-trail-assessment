b8ae1aa4399c75aced5aa2341a5052d6e298ba09
unknown <davarion_coleman@yahoo.com>
2020-12-22 06:43:27 -0600

    Oregon Trail Assessment

---
+++ main.js
@@ -0,0 +1,54 @@
+class Traveler {
+constructor (name) {
+    this.name = name;
+    this.food = 1;
+    this.isHealthy = true;
+}
+hunt () {
+    this.food +=2
+}
+eat () {
+    this.food -=1
+    if(this.food ===0) {
+        this.isHealthy = false;
+    } else {
+        this.food -= 1;
+    }
+}
+}
+
+class Wagon{
+    constructor(capacity)
+    {
+        this.capacity = capacity;
+        this.passengers = []
+    }
+
+    getAvailableSeatCount() {
+        return this.capacity - this.passengers.length;
+    }
+
+    join(traveler) {
+        if (this.passengers.length < this.capacity){
+            this.passengers.push(traveler);
+        }
+    }
+
+    shouldQuarantine() {
+        for(let i = 0; i < this.passengers.length; i++){
+            if (this.passengers[i].isHealthy === false) {
+                return true;
+            }
+        }
+        return false
+    }
+
+    totalFood() {
+        let total = 0
+        for(let i = 0; i < this.passengers.length; i++) {
+            total += this.passengers[i].food;
+        }
+        return total;
+    }
+
+}
